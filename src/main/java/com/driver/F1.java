472551250d197f207add515ad5f1575e89927eef
abhishekkmwt <kumawat.abhishek167@gmail.com>
2023-03-20 12:28:46 +0530

    try 1

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -9,8 +9,7 @@ public class F1 extends Car {
     }
 
     public void accelerate(int rate){
-        int newSpeed = 0; //set the value of new speed by using currentSpeed and rate
-        newSpeed=(getCurrentSpeed()+rate);
+        int newSpeed = rate + getCurrentSpeed(); //set the value of new speed by using currentSpeed and rate
         /**
          * speed 0: gear 1
          * speed 1-50: gear 1
@@ -26,22 +25,22 @@ public class F1 extends Car {
             stop();
             changeGear(1);
         }
-        else if(newSpeed>=1 && newSpeed<=50) {
+        else if(newSpeed<=50) {
             changeGear(1);
         }
-        else if(newSpeed>=51 && newSpeed<=100) {
+        else if(newSpeed<=100) {
             changeGear(2);
         }
-        else if(newSpeed>=101 && newSpeed<=150) {
+        else if(newSpeed<=150) {
             changeGear(3);
         }
-        else if(newSpeed>=151 && newSpeed<=200) {
+        else if(newSpeed<=200) {
             changeGear(4);
         }
-        else if(newSpeed>=201 && newSpeed<=250) {
+        else if(newSpeed<=250) {
             changeGear(5);
         }
-        else if(newSpeed>250){
+        else{
             changeGear(6);
         }
         //for all other cases, change the gear accordingly
